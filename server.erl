368b2b86a1c88411fbbf582c8094819943a0a5a1
Maximilian Algehed <m.algehed@gmail.com>
2016-03-14 20:34:12 +0100

    wat.jpeg

--- server.erl
+++ server.erl
@@ -74,7 +74,7 @@ client_listen_loop(PrivateKey, Sock, CBroker, Lst, Msgs) ->
                            case Message of 
                                 [0] -> client_recv(Msgs, CBroker),
                                        client_listen_loop(PrivateKey, Sock, CBroker, Cont, <<>>);
-                                _   -> client_listen_loop(PrivateKey, Sock, CBroker, Cont, <<Msgs/binary, Message>>)
+                                _   -> client_listen_loop(PrivateKey, Sock, CBroker, Cont, <<Msgs/binary, Message/binary>>)
                             end;
 
         error     -> client_disconnect(CBroker)
