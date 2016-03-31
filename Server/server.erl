ff407ac3cc460ef5936820399dbf160c9d87a585
Maximilian Algehed <m.algehed@gmail.com>
2016-03-31 19:37:41 +0200

    Print the error message

--- Server/server.erl
+++ Server/server.erl
@@ -30,7 +30,7 @@ broadcast(Msg, Broker)    -> Broker ! {broadcast, Msg}.
 server_listen(PrivateKey, PublicKey, LSock, Broker) ->
     case gen_tcp:accept(LSock) of
         {ok, CSock} -> spawn(fun() -> client:start_client(PrivateKey, PublicKey, CSock, Broker) end);
-        _           -> ok
+        {error, Error} -> io:format("Error: ~p", [Error])
     end,
     server_listen(PrivateKey, PublicKey, LSock, Broker).
 
